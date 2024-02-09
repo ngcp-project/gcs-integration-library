76098915fa14b1ae6d473c742d56ae6017fa9838
Daniel Pasion <dnpaxion@gmail.com>
2024-02-09 14:06:25 -0800

    Added NATS Telemetry Pub

--- Telemetry/NATS.py
+++ Telemetry/NATS.py
@@ -2,41 +2,41 @@ import os
 import asyncio
 import time
 import aioconsole
+import json
 
 import nats
 from nats.errors import TimeoutError
 
 #Initializing Telemetry Class
 class TelemetryNATS:
-    
-    #Creating the object
-    async def __init__(self):
-        self.speed = None
-        self.pitch = None
-        self.yaw = None
-        self.roll = None
-        self.altitude = None
-        self.batteryLife = None 
-        self.lastUpdated = None #TimeStamp
-        self.currentPosition = None #Coordinate Class
-        self.vehicleStatus = None #Status_Exum Class
         
+    #Creating the object
+    async def __init__(self):      
+        self.nc = None    
+        
+    async def setup_NATS(self):
         #Setting up the new connection
         servers = os.environ.get("NATS_URL", "nats://localhost:4222").split(",")
         #Connecting to the server
-        nc = await nats.connect(servers=servers)
-        #Return the new connection
-        self.nc = nc
-    
+        self.nc = await nats.connect(servers=servers)
+        
     #Function to run the telemetry connection.
-    async def publish_NATS(self,nc,node_name,speed, pitch, yaw, roll, altitude, batteryLife, lastUpdated, currentPosition, vehicleStatus):
-        print("Running")
-        counter = 0
-        while True:
-            current_time = time.time()
-            data = "{number: " + str(counter) + "}" + "\nCurrent time: " + str(current_time)
-            
-            await nc.publish(node_name, bytes(data,encoding='utf-8'))
-            time.sleep(2)
-            print("Current Iteration: " + str(counter))
-            counter += 1
+    async def publish_NATS(self,node_name,speed, pitch, yaw, roll, altitude, batteryLife, currentPosition, vehicleStatus):
+
+        #Creating the vehicle data dictionary
+        vehicle_data = {'speed': speed, 
+                        'pitch': pitch, 
+                        'yaw': yaw, 
+                        'roll': roll, 
+                        'altitude': altitude, 
+                        'batteryLife': batteryLife, 
+                        'lastUpdated': time.time(), 
+                        'currentPosition': currentPosition, 
+                        'vehicleStatus': vehicleStatus}
+        
+        #Turning vehicle_data into a json
+        jsondata = json.dumps(vehicle_data)
+        print
+        #Sending the data
+        await self.nc.publish(node_name, bytes(jsondata,encoding='utf-8'))
+        time.sleep(2)
