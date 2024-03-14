5bd1093bb710d5f9586dfe17341fdb9c08e6c84d
marked01one <minhkhoitran2k3@gmail.com>
2024-03-13 18:31:52 -0700

    Created instance of integration library

--- Tests/temp.py
+++
@@ -1,9 +0,0 @@
-import pika 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="192.168.0.101"))
-channel=connection.channel()
-vehicleName = "eru"
-channel.queue_declare(queue=f"telemetry_{vehicleName}")
-channel.basic_publish(exchange='', routing_key=f"telemetry_{vehicleName}", body='working now?')
-print("did send it")
-connection.close()
